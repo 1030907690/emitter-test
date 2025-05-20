65709d1bf7f464492b4207ee165b5275f6668317
zhouzhongqing <zhouzhongqing@qq.com>
2025-05-20 22:55:45 +0800

    feat 1

---
+++ src/main/java/com/zzq/controller/IndexController.java
@@ -0,0 +1,80 @@
+package com.zzq.controller;
+
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @description:
+ * @author: zzq
+ * @date: 4/29/2025 9:50 PM
+ */
+@RestController
+@RequestMapping("/api/index")
+public class IndexController {
+    private final Logger log = LoggerFactory.getLogger(IndexController.class);
+    private final ExecutorService executorService = Executors.newFixedThreadPool(2000);
+
+
+    private final List<String> replyData = Arrays.asList("我是", "您的AI助手", "有什么可以帮您", "我是", "您的AI助手", "有什么可以帮您");
+
+    @RequestMapping("/chat")
+    @CrossOrigin
+    public SseEmitter chat(String query) {
+        SseEmitter emitter = new SseEmitter();
+
+        executorService.execute(() -> {
+            try {
+                for (int i = 0; i < replyData.size(); i++) {
+                    String value = replyData.get(i);
+                    emitter.send(value.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+                    Thread.sleep(1000);
+                }
+
+                emitter.complete();
+            } catch (Exception e) {
+                log.error("其他的请求聊天异常 {}", e);
+                emitter.completeWithError(e);
+                throw new RuntimeException(e);
+            }
+        });
+        log.info("返回emitter");
+        return emitter;
+    }
+
+ /*   @RequestMapping(value = "/chat")
+    @CrossOrigin
+    public ResponseBodyEmitter chat(HttpServletResponse response) {
+        ResponseBodyEmitter emitter = new ResponseBodyEmitter(180000L);
+        executorService.execute(() -> {
+            try {
+                for (String value : replyData) {
+                    emitter.send("data: " + value + "\n\n");
+                    Thread.sleep(1000);
+                }
+                emitter.complete();
+            } catch (Exception e) {
+                log.error("其他的请求聊天异常 {}", e);
+                emitter.completeWithError(e);
+                throw new RuntimeException(e);
+            }
+        });
+
+        return emitter;
+    }*/
+
+}
